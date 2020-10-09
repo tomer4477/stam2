982d4798db9b83f99231ee27df64dc6c2c6db00d
tomerRoz <tomer4477@gmail.com>
2020-10-09 15:44:08 +0300

    add Animal

---
+++ src/src/Animal.java
@@ -0,0 +1,14 @@
+public class Animal {
+    private String name;
+
+    public Animal () {
+        System.out.println("i'm animal!");
+    }
+
+    public String getName (){
+        return name;
+    }
+    public void setName (String name){
+        this.name = name;
+    }
+}
